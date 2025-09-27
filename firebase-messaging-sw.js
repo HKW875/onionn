cc646224422732e65c351534a60eb497e51ffc5a
HKW875 <hkw875@hotmail.com>
2025-09-27 10:46:55 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,19 @@
 // firebase-messaging-sw.js
 
-importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDMc-z5-FwlbWwDt_dWdbk82SLpm9GCE38",
-  authDomain: "onionn-790da.firebaseapp.com",
-  projectId: "onionn-790da",
-  storageBucket: "onionn-790da.firebasestorage.app",
-  messagingSenderId: "838790352647",
-  appId: "1:838790352647:web:8d263b00650740b4157fa2"
+  messagingSenderId: '838790352647'
 });
 
 const messaging = firebase.messaging();
 
-// Background handler
-messaging.onBackgroundMessage((payload) => {
-  console.log("Background message received:", payload);
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: "/icons/android-launchericon-192-192.png"
-  });
+messaging.onBackgroundMessage(function(payload) {
+  const title = payload.notification?.title || 'Onionn';
+  const options = {
+    body: payload.notification?.body,
+    icon: '/icons/android-launchericon-192-192.png',
+  };
+  self.registration.showNotification(title, options);
 });
